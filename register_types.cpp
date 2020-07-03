d5c95a6c5d49e0283d129f26ec3481bcd2fa01a9
Relintai <relintai@gmail.com>
2020-07-03 13:04:17 +0200

    Fix build with tools=no.

--- register_types.cpp
+++ register_types.cpp
@@ -23,13 +23,12 @@ SOFTWARE.
 #include "register_types.h"
 
 #include "mesh_data_resource.h"
+#include "nodes/mesh_data_instance.h"
 
 #ifdef TOOLS_ENABLED
 #include "editor/editor_plugin.h"
 
 #include "plugin_collada/editor_plugin_collada_mdr.h"
-
-#include "nodes/mesh_data_instance.h"
 
 #include "plugin_gltf/editor_plugin_gltf_mdr.h"
 #endif
